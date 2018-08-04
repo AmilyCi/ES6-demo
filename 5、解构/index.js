12f8b2814ce60ebd19b4b3b88d2ebe8e66cff1d3
AmilyCi <cixinii@163.com>
2018-08-04 17:10:52 +0800

    ES6的基本语法

---
+++ 5、解构/index.js
@@ -0,0 +1,29 @@
+//ES5
+// const people = {
+//   name:'lux',
+//   age:20
+// };
+// const name = people.name;
+// const age =people.age;
+// console.log(name+'---'+age);
+
+//ES6
+// const people = {
+//     name:'lux',
+//     age:20
+// };
+// const {name,age}=people;
+// console.log(`${name}---${age}`);
+
+// const color = ['red','yellow'];
+// const [first,second] = color;
+// console.log(first);
+// console.log(second);
+
+//ES5
+// var body = request.body;
+// var username = body.username;
+// var password = body.password;
+
+//ES6
+// const {body,body:{username,password}}=request;
