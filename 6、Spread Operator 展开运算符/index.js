81ef319f028f68692dde8ab3821acc54924544c3
AmilyCi <cixinii@163.com>
2018-08-05 17:26:24 +0800

    ES6的基本语法

---
+++ 6、Spread Operator 展开运算符/index.js
@@ -0,0 +1,32 @@
+// const color = ['red','yellow'];
+// const colorful = [...color,'green','black'];
+// console.log(colorful);
+
+// const alp = {first:'a',second:'b'};
+// const alphabets = {...alp,third:'c'};
+// console.log(alphabets);
+
+// const number = [1,2,3,4,5];
+// const [first,...rest] = number;
+// console.log(rest);//->[2, 3, 4, 5]
+
+// const user = {
+//     username:'lux',
+//     gender:'female',
+//     age:'19',
+//     address:'peking'
+// };
+// const {username,...rest} = user;
+// console.log(rest);//->{gender: "female", age: "19", address: "peking"}
+
+// const first = {
+//     a:1,
+//     b:2,
+//     c:6
+// };
+// const second = {
+//     c:3,
+//     d:4
+// };
+// const total = {...first,...second};
+// console.log(total);//{a: 1, b: 2, c: 3, d: 4}
